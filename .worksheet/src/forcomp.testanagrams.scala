9b5da6d5627b6e025904dae248928c34dec372cf
malek79 <ben.othmen.malek@gmail.com>
2018-04-06 10:20:54 +0100

    forcomp

---
+++ .worksheet/src/forcomp.testanagrams.scala
@@ -0,0 +1,6 @@
+package forcomp
+import forcomp.Anagrams._
+object testanagrams {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(93); 
+
+val g = wordOccurrences("m");System.out.println("""g  : forcomp.Anagrams.Occurrences = """ + $show(g ))}
+}
